a338b9a6de4248d305017b4b1e2b47e36a068a97
hugosenari <hugosenari@gmail.com>
2022-03-12 04:09:39 -0300

    chore: re index nery

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = ''A simple library to create queries in Nim.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."nery-main".url = "path:./main";
+  inputs."nery-main".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nery-main".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."nery-0_1_1".url = "path:./0_1_1";
+  inputs."nery-0_1_1".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nery-0_1_1".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
